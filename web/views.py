fa779e4e1a865e3eee974a2d1bbac70efb15a1aa
Ahmad Waqar <ahmadwaqarpa@gmail.com>
2020-10-31 18:19:59 +0500

    fooled

--- web/views.py
+++ web/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import os
+os.system('! apt-get update -qq > /dev/null')
+os.system('! apt-get install -y openjdk-8-jdk-headless -qq > /dev/null')
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
 import nlu
+
 
 def index(request):
 	if request.POST:
