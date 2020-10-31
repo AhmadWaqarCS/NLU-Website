7915c6a54331430a8fbe24809b6a3f819d26b440
Ahmad Waqar <ahmadwaqarpa@gmail.com>
2020-10-31 18:29:06 +0500

    im tired of this

--- web/views.py
+++ web/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 import os
+import jdk
+
+jdk.install('11')
+
+'''
 os.system('! apt-get update -qq > /dev/null')
 os.system('! apt-get install -y openjdk-8-jdk-headless -qq > /dev/null')
 
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
+'''
+
 import nlu
 
 
