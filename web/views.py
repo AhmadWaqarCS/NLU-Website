4023330c60028b168718dbcb077de7a53414d25b
Ahmad Waqar <ahmadwaqarpa@gmail.com>
2020-10-31 17:06:31 +0500

    wierd changes

--- web/views.py
+++ web/views.py
@@ -5,8 +5,8 @@ def index(request):
 	if request.POST:
 		ty = request.POST['type']
 		line = request.POST['value']
-		pipeline =nlu.load("sentiment")
-		prediction= pipeline.predict(line)
+		pipeline = nlu.load(ty)
+		prediction = pipeline.predict(line)
 		return render(request, 'web/index.html', {'answer': prediction})
 	else:
 		return render(request, 'web/index.html', {})
